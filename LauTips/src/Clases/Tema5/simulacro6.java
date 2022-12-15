bbda91abdb9bd7a744644d05449452afd94eba0c
LauraCGlez <1234solojuegos@gmail.com>
2022-12-15 08:48:36 +0100

    commit prueba

--- LauTips/src/Clases/Tema5/simulacro6.java
+++ LauTips/src/Clases/Tema5/simulacro6.java
@@ -1,6 +1,16 @@
 package Clases.Tema5;
 
+import java.util.Scanner;
+
 //Crear una función a la que se le pase una tabla y devuelva otra igual pero
-//con un elemento más (que también se le pasa como parámetro)
+//con un elemento más (que también se le pasa como parámetro).
 public class simulacro6 {
+
+    public static void main(String[] args) {
+
+        int[] tabla1 = new int[10];
+    }
+
 }
+
+
